7e1cb7c07fb233242f27511770ffb1dea9c0dbb3
Denis <denis.voltmann@gmail.com>
2025-11-23 21:58:06 +0100

    Update build settings

--- imx-setup-release.sh
+++ imx-setup-release.sh
@@ -170,8 +170,8 @@ echo 'INHERIT += "rm_work"' >> conf/local.conf
 echo 'INHERIT:remove = "ccache"' >> conf/local.conf
 
 # Parallelism
-echo 'BB_NUMBER_THREADS = "8"' >> conf/local.conf
-echo 'PARALLEL_MAKE = "-j 8"' >> conf/local.conf
+echo 'BB_NUMBER_THREADS = "12"' >> conf/local.conf
+echo 'PARALLEL_MAKE = "-j 16"' >> conf/local.conf
 #
 # === END CUSTOM local.conf ADDITIONS ===
 #
